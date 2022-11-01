d281d7fa4b83996d20d691140c5e24313361e462
Raquel <raquel.chacim@hotmail.com>
2022-11-01 12:22:35 +1100

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+let a = 2000;
+let b = 100;
+
+const divide = () => a / b;
+console.log(divide());
+
+let x = 2;
+
+const square = () => x * 2;
+console.log(square());
+
+let a = 3;
+let b = 4;
+
+const add = () => a + b;
+console.log(add());
